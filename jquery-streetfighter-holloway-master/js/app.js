8765c2f80380b3817774a590e069d6eca3b2bf3f
Benjamin Holloway <bholloway546@gmail.com>
2015-04-22 01:35:21 -0400

    Keydown not Keyup

--- jquery-streetfighter-holloway-master/js/app.js
+++ jquery-streetfighter-holloway-master/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$('.ryu').on('mouseenter',function(){
 		$('.ryu-still').hide()
 		$('.ryu-ready').show()
@@ -8,13 +9,47 @@ $(document).ready(function(){
 		$('.ryu-ready').hide()
 	});
 	$('.ryu').on('mousedown',function(){
+		playHadouken()
 		$('.ryu-ready').hide()
 		$('.ryu-throwing').show()
-		$('.hadouken').show()
-
+		$('.hadouken').finish().show().animate(
+			{'left':'1020px'},
+			750,
+			function(){
+				$('.hadouken').hide()
+				$('.hadouken').css('left','520px');
+			}
+			);
 	});
 	$('.ryu').on('mouseup',function(){
 		$('.ryu-ready').show()
 		$('.ryu-throwing').hide()
 	});
-});	
+
+	$(document).on('keydown',function(e) {
+    if (e.keyCode == 88) {
+      /*playJazz();*/
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();  
+  }
+}); /*.on('keyup',
+	/*$('.ryu').on('keydown', function(e){
+		if(e.keyCode == 88){
+			playJazz()
+			$('.ryu-ready').hide()
+			$('.ryu-cool').show()
+		}
+	});*/
+});
+
+function playHadouken(){
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+  	$('#hadouken-sound')[0].play();
+}
+
+/*function playJazz(){
+	$('#jazz-sound')[0].volume = 0.5;
+	$('#jazz-sound')[0].load();
+  	$('#jazz-sound')[0].play();
+}*/
